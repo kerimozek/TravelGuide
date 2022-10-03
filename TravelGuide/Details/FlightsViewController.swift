8f19644e75bc8a1ff2f9a20be73e4a08cd7e950e
Mehmet Kerim ÖZEK <mehmetkerimozek@gmail.com>
2022-10-03 11:49:29 +0300

    flights screen designed
    flights screen designed

---
+++ TravelGuide/Details/FlightsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FlightsViewController.swift
+//  TravelGuide
+//
+//  Created by Mehmet Kerim ÖZEK on 2.10.2022.
+//
+
+import UIKit
+
+class FlightsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func backButtonClicked(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+}
