79697a7dc57206a2f3bf7e858b46fc60b37b8fbf
Mehmet Kerim ÖZEK <mehmetkerimozek@gmail.com>
2022-10-03 11:49:10 +0300

    bookmarks screen designed
    bookmarks screen designed

---
+++ TravelGuide/Bookmarks/BookmarksViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BookmarksViewController.swift
+//  TravelGuide
+//
+//  Created by Mehmet Kerim ÖZEK on 2.10.2022.
+//
+
+import UIKit
+
+class BookmarksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    // MARK: - BOOKMARKS
+
+}
