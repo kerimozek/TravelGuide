31ac4d8379f1345657e592dd3947b4d9572587b0
Mehmet Kerim ÖZEK <mehmetkerimozek@gmail.com>
2022-10-04 20:18:17 +0300

    added toppick collection view xib
    added toppick collection view xib

---
+++ TravelGuide/Main/CollectionViewCell/TopPickCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  TopPickCollectionViewCell.swift
+//  TravelGuide
+//
+//  Created by Mehmet Kerim ÖZEK on 3.10.2022.
+//
+
+import UIKit
+
+class TopPickCollectionViewCell: UICollectionViewCell {
+
+    
+    @IBOutlet weak var titleLabelCell: UILabel!
+    @IBOutlet weak var categoryLabelCell: UILabel!
+    @IBOutlet weak var imageViewCell: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+      
+    }
+
+}
